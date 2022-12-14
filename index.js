75f13ae321277e58b7d49fbf5242d3c114c4b572
Victor <thelegendofvictor@gmail.com>
2022-12-14 15:37:23 -0600

    made changes in 3 files

--- index.js
+++ index.js
@@ -7,17 +7,22 @@
 //   // counter.innerHTML++;
 // });
 
-// const btnSmall = document.getElementById("small");
-// const btnBig = document.getElementById("big");
-// const placeholder = document.querySelector(".placeholder");
+const btnSmall = document.getElementById("small");
+const btnBig = document.getElementById("big");
+const placeholder = document.querySelector(".placeholder");
+let counter = 50;
+btnSmall.addEventListener("click", function () {
+  if (counter > 1) {
+    placeholder.style.fontSize = counter-- + "px";
+  }
+  // placeholder.style.fontSize = "10px";
+});
 
-// btnSmall.addEventListener("click", function () {
-//   placeholder.style.fontSize = "10px";
-// });
-
-// btnBig.addEventListener("click", function () {
-//   placeholder.style.fontSize = "100px";
-// });
+btnBig.addEventListener("click", function () {
+  if (counter < 100) {
+    placeholder.style.fontSize = counter++ + "px";
+  }
+});
 
 // const btn = document.querySelector(".btn");
 // // const form = document.querySelector(".form");
@@ -35,9 +40,33 @@
 //   body.style.backgroundColor = input;
 // });
 
-const up = document.getElementById("up");
-const down = document.getElementById("down");
-const left = document.getElementById("left");
-const right = document.getElementById("right");
+// const up = document.getElementById("up");
+// const down = document.getElementById("down");
+// const left = document.getElementById("left");
+// const right = document.getElementById("right");
 
-const crane = document.querySelector(".crane");
+// const crane = document.querySelector(".crane");
+// let counter = 0;
+// up.addEventListener("click", function () {
+//   crane.style.marginTop = counter-- + "px";
+// });
+
+// down.addEventListener("click", function () {
+//   crane.style.marginTop = counter++ + "px";
+// });
+// let counter1 = 0;
+// left.addEventListener("click", function () {
+//   crane.style.marginLeft = counter1-- + "px";
+// });
+
+// right.addEventListener("click", function () {
+//   crane.style.marginLeft = counter1++ + "px";
+// });
+
+// const input = document.querySelector(".input");
+// const btn = document.querySelector(".btn");
+// const output = document.querySelector(".output");
+
+// btn.addEventListener("click", function () {
+//   output.innerHTML = Math.trunc(Math.random() * input.value + 1);
+// });
