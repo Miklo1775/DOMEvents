9b753839ca10d45be11cfb8b2e0bef923719c4dd
Victor <thelegendofvictor@gmail.com>
2022-12-14 14:47:27 -0600

    changes to 3 files

---
+++ index.js
@@ -0,0 +1,43 @@
+// const cookieImg = document.querySelector(".cookie");
+// const counter = document.querySelector(".counter");
+
+// let counter1 = 0;
+// cookieImg.addEventListener("click", function () {
+//   counter.innerHTML = counter1++;
+//   // counter.innerHTML++;
+// });
+
+// const btnSmall = document.getElementById("small");
+// const btnBig = document.getElementById("big");
+// const placeholder = document.querySelector(".placeholder");
+
+// btnSmall.addEventListener("click", function () {
+//   placeholder.style.fontSize = "10px";
+// });
+
+// btnBig.addEventListener("click", function () {
+//   placeholder.style.fontSize = "100px";
+// });
+
+// const btn = document.querySelector(".btn");
+// // const form = document.querySelector(".form");
+// const body = document.querySelector("body");
+// const input = document.getElementById("input");
+
+// function returnText() {
+//   let input = document.getElementById("input").value;
+// }
+
+// function getInput(input) {}
+
+// btn.addEventListener("click", function () {
+//   let input = document.getElementById("input").value;
+//   body.style.backgroundColor = input;
+// });
+
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+const crane = document.querySelector(".crane");
